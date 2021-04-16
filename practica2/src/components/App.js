a2ab8d620077f30675d21f402b138fc9dd22dfbc
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-15 20:17:57 -0500

    Se creo el Layout para usarlo en varias paginas

--- practica2/src/components/App.js
+++ practica2/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Juegos from './Juegos';
+import Layout from './Layout';
 import Navbar from './Navbar';
 
 const App = ()=>{
     return(
-        <Juegos/>
+        <Layout>
+            <Juegos/>
+        </Layout>
     );
 }
 
