d60c2ab497238bf55510f7dababbb9984b25058c
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-19 18:53:34 -0500

    Css para pagina about

--- practica2/src/pages/About.js
+++ practica2/src/pages/About.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import './style/About.css';
 
 export default class About extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <div className="about">
-                    <h1>Empresa de videojuegos</h1>
+                    <h1>Konami Holdings Corporation</h1>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae tincidunt nisl. Quisque nisi odio, convallis sit amet sapien eu, egestas volutpat mauris. Proin pellentesque, massa in mattis lacinia, metus risus elementum lacus, nec vehicula massa leo nec tellus. Curabitur eu nunc mollis, egestas augue id, imperdiet turpis. Donec id eleifend est. Aliquam pretium, lacus vitae molestie imperdiet, lacus neque venenatis massa, sit amet auctor leo diam vel lorem. Nulla sed nibh nulla. Duis cursus a purus sed posuere. Quisque at hendrerit orci. Vestibulum justo quam, placerat eget semper ac, iaculis et massa. Quisque ex turpis, aliquam ut eleifend a, imperdiet a nisl. Donec eget nibh aliquet, sollicitudin eros a, sollicitudin massa.
-                        Sed rutrum efficitur condimentum. Praesent volutpat neque nisl, quis varius elit egestas nec. Aliquam mollis, risus ut cursus blandit, nulla sapien sagittis leo, in malesuada lectus nulla vitae justo. Curabitur suscipit euismod odio id fermentum. Vestibulum sed efficitur diam. Integer in sapien neque. Nunc gravida eros at turpis tempor, ac ultrices urna mollis. Etiam eu augue eget ipsum commodo dapibus.
+                    Konami Holdings Corporation (コナミホールディングス株式会社 Konami Hōrudingusu Kabushiki-gaisha?), comúnmente conocido como Konami, es una empresa de desarrollo de juguetes, cartas coleccionables, anime, tokusatsu, máquinas tragaperras y videojuegos. Fue fundada en 1969 como un negocio de reparación y de jukeboxes en Osaka, Japón, por Kagemasa Kozuki, quien es todavía su presidente y director ejecutivo.
+                    </p>
+                    <p>
+                    El nombre "Konami" es una conjunción de los nombres Kagemasa Kozuki, Yoshinobu Nakama, Hiro Matsuda, y Shokichi Ishihara, quienes fueron los socios de Kozuki y los fundadores originales de Konami Industry Co., Ltd en 1973. Konami también significa «olas pequeñas». Tiene actualmente sus bases en Tokio y adicionalmente opera clubes de salud y ejercicio en Japón.
+                    </p>
+                    <p>
+                    Konami ha sido el responsable de la creación de 6 de las franquicias más exitosas en la historia de los videojuegos: Castlevania, Contra, Metal Gear Solid, Silent Hill, Dance Dance Revolution (que fue el parte aguas para la creación de este tipo de juegos como Pump It Up de Andamiro) y Pro Evolution Soccer (desde su primer videojuego, International Superstar Soccer), así como ser la creadora de videojuegos exitosos, basados en series, caricaturas y películas, tales son los casos de los videojuegos de las caricaturas Tiny Toon Adventures, Animaniacs y Tortugas Ninja, así como el videojuego Batman Returns, basado en la película homónima, y haber desarrollado el considerado mejor videojuego de arcade de todos los tiempos: The Simpsons Arcade Game. Aunado a eso, también ha sido la creadora de la franquicia de cartas coleccionable de Yu-Gi-Oh!, basados en el popular ánime japonés, logrando un gran éxito (y ser la única que le pudo hacer frente al éxito del también juego de cartas Magic: El encuentro).
                     </p>
                 </div>
             </React.Fragment>
