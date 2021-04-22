a2f169f8be31a844a685dc669c3f6e554cc15577
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-22 00:11:40 -0500

    Estilos de css a Juegos

--- practica2/src/components/Juegos.js
+++ practica2/src/components/Juegos.js
@@ -1,26 +1,29 @@
 import React from "react";
 import './style/Juegos.css';
-import img from '../img/cnav.png';
 
-export default class Juegos extends React.Component{
-    constructor(props){
+export default class Juegos extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
             list: require('../api/datos.json')
         }
     }
-    render(){
-        return(
+
+    render() {
+        return (
             <React.Fragment>
-                <div className="card-juegos">
-                    <div className="card-juegos-img">
-                        <img src={img} alt="imagen"></img>
-                    </div>
-                    <div className="card-juegos-textos">
-                        <h2>Titulo</h2>
-                        <span>Se creo</span>
-                        <a>Conocer mas</a>
-                    </div>
+                <div className="grid-juegos">
+                    {this.state.list.map((element) => {
+                        return (
+                            <div key={element.id} className="card-juegos">
+                                <div className="card-juegos-textos">
+                                    <h2>{element['titulo']}</h2>
+                                    <span>{element['fecha_creacion']}</span>
+                                    <p>{element['descripcion']}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </React.Fragment>
         );
