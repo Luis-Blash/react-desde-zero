2775e0fedab6c6c3cca6e3f8d6cd4c9c0304c277
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-21 22:03:38 -0500

    Agregado los datos json

--- practica2/src/components/Juegos.js
+++ practica2/src/components/Juegos.js
@@ -3,6 +3,12 @@ import './style/Juegos.css';
 import img from '../img/cnav.png';
 
 export default class Juegos extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            list: require('../api/datos.json')
+        }
+    }
     render(){
         return(
             <React.Fragment>
